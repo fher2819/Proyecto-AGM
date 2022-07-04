10ccda744d88536a6bc9659e7281727a1cfa5275
Fher2819 <francisco.huembes71@std.uni.edu.ni>
2022-07-04 03:09:45 -0600

    Agregar admin

---
+++ js/adminDestinos.js
@@ -0,0 +1,9 @@
+const modalShow = document.querySelector('#modalDetalles');
+
+const openModal = () => {
+    modalShow.style.display = 'flex';
+}
+
+const closeModal = () => {
+    modalShow.style.display = 'none';
+}
